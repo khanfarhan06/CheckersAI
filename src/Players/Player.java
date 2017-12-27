009a2b6a108c4701b260e0db19b138fb86380485
Md Farhan Khan <khanfarhan281@gmail.com>
2017-12-27 23:57:22 +0530

    Comments added

--- src/Players/Player.java
+++ src/Players/Player.java
@@ -4,17 +4,36 @@ import Checkers.Alliance;
 import Checkers.CheckersBoard;
 import Checkers.Move;
 
+/***
+ * Player class is the abstract class that acts as the super class of all the Players in the game
+ * It has a member variable which corresponds to the alliance of the player
+ * It defines the getAlliance() and setAlliance() getter setter methods for the alliance variable
+ * It provides an abstract bestMove() which has to be implemented by all the subclasses of this Player class
+ */
 public abstract class Player {
     private Alliance alliance;
 
+    /***
+     * Getter method for alliance member variable
+     * @return The alliance of this Player
+     */
     public Alliance getAlliance() {
         return alliance;
     }
 
+    /***
+     * Setter method for the alliance member variable
+     * @param alliance The alliance to which the alliance of this Player is to be set to
+     */
     public void setAlliance(Alliance alliance){
         this.alliance = alliance;
     }
 
+    /***
+     * Returns the best move in the current board position according to this Player
+     * @param checkersBoard The current board position where the best move is to be found
+     * @return  A Move which is the best move in the given board position according to this Player
+     */
     abstract public Move bestMove(CheckersBoard checkersBoard);
 
 }
--- src/Players/RandomPlayer.java
+++ src/Players/RandomPlayer.java
@@ -6,17 +6,25 @@ import Checkers.Move;
 import java.util.List;
 import java.util.Random;
 
+/***
+ * RandomPlayer class represents a Player which makes a Random move in the list of possible moves in the current board position
+ * It is a subclass of the Payer abstract class, so it provides an implementation of the bestMove() method
+ */
 public class RandomPlayer extends Player {
 
-    private final Random randomGenerator = new Random();
+    private final Random randomGenerator = new Random();    //A random generator object
 
     @Override
-    //TODO jumpmoves not correctly implemented
     public Move bestMove(CheckersBoard checkersBoard) {
+        //get all possible moves in the current board position
         List<Move> legalMoves = checkersBoard.getAllMoves();
-        if (legalMoves.size()==0)
+        //If no possible move was found return a null value,thsi is used in Game class's start() to find if the Player has no possible moves
+        if (legalMoves.size()==0)   //Condition to check if no possible moves where present
             return null;
+        //Generate a random index from 0 to the size of the list of the legal moves
         int index = randomGenerator.nextInt(legalMoves.size());
+
+        //Return the move at the generated random index
         return legalMoves.get(index);
     }
 }
